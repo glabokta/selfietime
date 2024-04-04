5a327c94100d3c26d4e2ee197b2d41d466c47de9
glabokta <124001355+glabokta@users.noreply.github.com>
2024-04-04 10:00:44 -0700

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-import logo from "https://qdev.s3.amazonaws.com/303/A/471068303/A/c0d4abd6-4f7b-11e4-8a0b-123140053ab5-471068303/DemoBank-Logo.png";
+//import logo from "https://qdev.s3.amazonaws.com/303/A/471068303/A/c0d4abd6-4f7b-11e4-8a0b-123140053ab5-471068303/DemoBank-Logo.png";
 
 const Hero = () => (
   <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
+    <img className="mb-3 app-logo" src="https://qdev.s3.amazonaws.com/303/A/471068303/A/c0d4abd6-4f7b-11e4-8a0b-123140053ab5-471068303/DemoBank-Logo.png" alt="React logo" width="120" />
     <h1 className="mb-4">New Banc KYC Demo</h1>
 
     <p className="lead">
