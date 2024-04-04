f8d03f456e4b0280f3ba16b9adfc2d088645d05e
glabokta <124001355+glabokta@users.noreply.github.com>
2024-04-04 09:22:28 -0700

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,11 +5,11 @@ import logo from "../assets/logo.svg";
 const Hero = () => (
   <div className="text-center hero my-5">
     <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+    <h1 className="mb-4">New Banc KYC Demo</h1>
 
     <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
+      This sample app demonstrates the AuthID ID Proofing capabilities through 
+      CIC martketplace integration. For more details pls goto <a href="https://manage.auth0.com/dashboard/uk/glab-pkey/marketplace/integrations/verified-biometric-mfa">AuthID @ CIC Marketplace</a>
     </p>
   </div>
 );
